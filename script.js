cc2649ac501b283abf2a8213ec0f323167597400
Samuel Seguins <85202953+samseg01@users.noreply.github.com>
2023-11-27 10:22:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 const btnOxe = document.getElementById("btn1");
 const btnNovin = document.getElementById("btn2");
+const btnInicia = document.getElementById("inicia");
 
 const btnNo = document.getElementById("no");
 const btnSim = document.getElementById("sim");
 
-
+const telaBtn = document.getElementById("selec-button");
 const tela1 = document.getElementById("pt1");
 const tela2 = document.getElementById("pt2");
 const tela3 = document.getElementById("pt3");
 
-const musica1 = new Audio('notas/la-.mp3');
-const musica2 = new Audio('notas/la-.mp3');
-const musica3 = new Audio('notas/la-.mp3');
+const musica1 = new Audio('mcs1.mp3');
+const musica2 = new Audio('mcs2.mp3');
+const musica3 = new Audio('mcs3.mp3');
 
-
+btnInicia.addEventListener('click', function(){
+    musica1.play();
+    tela1.style.display = "flex";
+    tela2.style.display = "none";
+    tela3.style.display = "none";
+    btnInicia.style.display = "none";
+    telaBtn.style.display = "none";
+})
 
 btnOxe.addEventListener('click', function(){
+    musica1.pause();
+    musica2.play();
     tela1.style.display = "none";
     tela2.style.display = "flex";
     tela3.style.display = "none";
+    telaBtn.style.display = "none";
 })
 btnNovin.addEventListener('click', function(){
+    musica1.pause();
+    musica2.play();
     tela1.style.display = "none";
     tela2.style.display = "flex";
     tela3.style.display = "none";
+    telaBtn.style.display = "none";
 })
 
 btnNo.addEventListener('mouseover', function(){
@@ -37,6 +51,8 @@ function random(limite){
 }
 
 btnSim.addEventListener('click', function(){
+    musica2.pause();
+    musica3.play();
     tela1.style.display = "none";
     tela2.style.display = "none";
     tela3.style.display = "flex";
